8fdbed930444dd6f36bd1742cc5fed7c2d2e8955
lta-anlee <thanhan9478@gmail.com>
2023-09-06 14:58:05 +0700

    buoi 4

---
+++ Bai7/Bai7/HinhTron.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    class HinhTron
+    {
+        public ToaDo tam;
+        public double banKinh;
+
+        public ToaDo Tam
+        {
+            get { return tam; }
+            set { tam = value; }
+        }
+
+        public double BanKinh
+        {
+            get { return banKinh; }
+            set { banKinh = value; }
+        }
+
+        public HinhTron(ToaDo tam, double bankinh)
+        {
+            this.tam = tam;
+            this.banKinh = bankinh;
+        }
+
+        public HinhTron() { }
+
+        public double tinhDienTich()
+        {
+            return BanKinh * BanKinh * 3.14;
+        }
+
+        public double tinhChuVi()
+        {
+            return BanKinh * 2 * 3.14;
+        }
+
+    }
+}
---
+++ Bai7/Bai7/ToaDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+    class ToaDo
+    {
+        public string ten;
+        public float x, y;
+
+        public ToaDo(String Ten)
+        {
+            this.ten = Ten;
+        }
+        public ToaDo( float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public ToaDo()
+        {
+
+        }
+        public string Ten
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
